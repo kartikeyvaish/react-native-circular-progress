affcbbee42a1ed41ae297baa725a92095ca28003
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-03-25 00:23:08 +0530

    :tada: created package

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Packages imports
+import { StyleSheet, Text, View } from "react-native";
 
+// Local imports
+import CircularProgress from "./CircularProgress";
+
+// exporting the default component
 export default function App() {
+  // render
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flex: 1, marginBottom: 30, alignItems: "center" }}>
+        <Text style={styles.heading}>Default View</Text>
+        <CircularProgress progress={40} />
+      </View>
+
+      <View style={{ flex: 1, marginBottom: 30, alignItems: "center" }}>
+        <Text style={styles.heading}>Change Colors</Text>
+        <CircularProgress progress={60} outerCircleColor="brown" progressCircleColor="orange" />
+      </View>
+
+      <View style={{ flex: 1, marginBottom: 30, alignItems: "center" }}>
+        <Text style={styles.heading}>Show/Remove Label</Text>
+        <CircularProgress progress={40} showLabel={false} />
+      </View>
+
+      <View style={{ flex: 1, marginBottom: 30, alignItems: "center" }}>
+        <Text style={styles.heading}>Increase Width of Stroke</Text>
+        <CircularProgress progress={40} strokeWidth={20} />
+      </View>
     </View>
   );
 }
 
+// styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+  },
+  heading: {
+    color: "white",
+    marginBottom: 20,
+    fontSize: 15,
   },
 });
